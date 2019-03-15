1b7192ef9629fea8ff1e1f7838283bff74e88528
Simo Lemhandez <s.lemhandez@gmail.com>
2019-03-15 15:49:44 +0000

    changed the value

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
     @user = current_user
     @cases = Case.all
     @last_attempt = Attempt.where(user_id: current_user).last
+    @last_attempt_completed = Attempt.all.where(completed: true, user_id: current_user).last
   end
 end
