4e74736c5d4c33a68ac5a78dad4e918dbdfa51b4
Simo Lemhandez <s.lemhandez@gmail.com>
2019-05-07 12:26:33 +0000

    m

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_153127) do
+ActiveRecord::Schema.define(version: 2019_05_07_122538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,7 +122,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_153127) do
     t.integer "non_profit", default: 0
     t.integer "public_sector", default: 0
     t.boolean "suscribed", default: false
-    t.integer "price_cents", default: 10000, null: false
+    t.integer "price_cents", default: 6000, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
